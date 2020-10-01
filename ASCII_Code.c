12fb9a15f4facc4284aa931de3c3755f4a38f40e
Kishan Lal <72204965+Kishan5406@users.noreply.github.com>
2020-10-01 21:53:35 +0530

    Update and rename ASCII_code.c to ASCII_Code.c

--- ASCII_Code.c
+++ ASCII_Code.c
@@ -7,6 +7,6 @@ int main(void)
     printf("c in ASCII is %d\n", c);
     printf("c the character %c\n", c);
     printf("Three consecutive chars are : %c %c %c\n", c, c + 1, c + 2);
-    printf("Three bell rings chars are : %c%c%c\n", 'a', '\a', '\a');
+    printf("Three bell rings chars are : %c %c %c\n", 'a', '\a', '\a');
     return 0;
 }
